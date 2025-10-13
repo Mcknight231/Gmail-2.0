05daf6efe921c5daa50e06270ee86ca233df9734
McKnight <babaevtamerlan3@gmail.com>
2025-10-13 12:16:40 +0400

    New

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counterSlice';
+import mailReducer from '../features/mailSlice';
 
 
 export default configureStore({
     reducer: {
-        counter: counterReducer,
+        mail: mailReducer,
     },
 });
